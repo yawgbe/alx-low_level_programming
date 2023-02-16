009b72047601653a08afd88758bc9a9cca74d3bf
yawgbe <ynyalemegbe@gmail.com>
2023-02-16 07:09:08 -0800

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,9 @@
 /**
  * main - Using puts to print a line in C programming
  * Return: 0
- * /							
- int main(void)
- {
+ */
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
