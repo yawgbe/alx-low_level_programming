9f1c1534f98bfa45722e364dadb44044f1976f2d
yawgbe <ynyalemegbe@gmail.com>
2023-02-16 13:28:57 -0800

    6

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,8 +3,10 @@
  * main - A program that prints with printf function
  * Return 0
  */
+
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+{	
+	printf("with proper grammar, but the outcome is a piece of art,\n")	
+	return (0);
 }
+
