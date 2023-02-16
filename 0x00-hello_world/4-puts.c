327ba9d5e3137847cb91f63d83fb565675eff58e
yawgbe <ynyalemegbe@gmail.com>
2023-02-16 06:59:07 -0800

    betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Using puts to print a line in C programming
+ * Return: 0
+ * /							
+ int main(void)
+ {
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
